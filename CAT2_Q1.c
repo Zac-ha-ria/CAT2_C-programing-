a723f30b5c56665f03f5fc892bec71d9619c2a43
Zac-ha-ria <njihiazacharia0@gmail.com>
2024-11-07 20:15:19 +0300

    Add files via upload

---
+++ CAT2_Q1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct employee{
+char name[25];
+int Id;
+char department[20];
+float salary;
+char email[50];
+}
+int main()
+{
+strcpy(employee.name,"John Doe");
+employee.Id=12345;
+strcpy(employee.department,"human resource");
+employee.salary=55000.50
+strcpy(employee.email,"john.doe@company.com);
+printf("name:%s\n," employee.name);
+printf("Id:%d\n, "employee.Id);
+printf("department:%s\n," employee.department);
+printf("salary:%2f\n," employee.salary);
+"
+    return 0;
+}
